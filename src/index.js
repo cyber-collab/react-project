e1235a9ed8ffd66d52d7c7e7e7fe893133b66c18
Den <denyszkramar@gmail.com>
2024-04-08 22:12:16 +0300

    feat: add simple counter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
+ReactDOM.render(
+    <DevSupport ComponentPreviews={ComponentPreviews}
+                useInitialHook={useInitial}
+    >
+        <App></App>
+    </DevSupport>,
+    document.getElementById('root')
+)

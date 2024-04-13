60057c24c5537a781c978286807d07d780357720
Den <denyszkramar@gmail.com>
2024-04-13 19:38:46 +0300

    refactor: sorting for posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import React, {useState} from "react";
+import React, {useMemo,useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/Select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
 
@@ -14,40 +14,35 @@ function App() {
         {id: 4, title: 'Laravel the be21s3', body: 'Description'}
     ]);
 
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [selectedSort, setSelectedSort ] = useState('')
+    const [filter, setFilter] = useState({sort: '', search: ''})
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    const sortedPosts = useMemo(() => {
+        if(filter.sort) {
+            console.log('bla bla')
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
+
+    const sortedAndSearchPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.search));
+    }, [filter.search, sortedPosts]);
+
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortPost = (sort) => {
-        setSelectedSort(sort)
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <div>
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPost}
-                    defaultValue="Sorting"
-                    options={[
-                        {value: 'title', name: 'By title'},
-                        {value: 'body', name: 'By description'}
-
-                    ]}
-                />
-            </div>
-            {posts.length !== 0 ? <PostList remove={removePost} posts={posts} title = "Posts list" /> : <h1 style={{textAlign: "center"}}>Posts not found</h1>}
+            <PostFilter filter={filter} setFilter={setFilter} />
+            {posts.length !== 0 ? <PostList remove={removePost} posts={sortedAndSearchPosts} title = "Posts list" /> : <h1 style={{textAlign: "center"}}>Posts not found</h1>}
         </div>
     )
 }
---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MyInput from "./UI/Input/MyInput";
+import MySelect from "./UI/Select/MySelect";
+
+const PostFilter = ({filter, setFilter}) => {
+    return (
+        <div>
+            <MyInput
+                value={filter.search}
+                onChange={e => setFilter({...filter, search: e.target.value})}
+                placeholder="Search..."
+            />
+            <MySelect
+                value={filter.sort}
+                onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+                defaultValue="Sorting"
+                options={[
+                    {value: 'title', name: 'By title'},
+                    {value: 'body', name: 'By description'}
+
+                ]}
+            />
+        </div>
+    );
+};
+
+export default PostFilter;
